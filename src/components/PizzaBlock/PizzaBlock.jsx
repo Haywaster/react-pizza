fecbde580a5f1c140ba0c3fb2e6c036b601b20c5
Haywaster <haywaster3@gmail.com>
2023-07-03 23:54:57 +0700

    ref: adaptive layout

--- src/components/PizzaBlock/PizzaBlock.jsx
+++ src/components/PizzaBlock/PizzaBlock.jsx
@@ -1,68 +1,73 @@
 import React, { useState } from 'react';
 
 const PizzaBlock = pizzaData => {
-  let typeNames = ['тонкое', 'традиционное'];
+	let typeNames = ['тонкое', 'традиционное'];
 
-  const [pizzaCount, setPezzaCount] = useState(null);
-  const [sizeIndex, setSizeIndex] = useState(0);
-  const [typeIndex, setTypeIndex] = useState(0);
+	const [pizzaCount, setPezzaCount] = useState(null);
+	const [sizeIndex, setSizeIndex] = useState(0);
+	const [typeIndex, setTypeIndex] = useState(0);
 
-  const onClickAdd = () => {
-    setPezzaCount(prev => prev + 1);
-  };
+	const onClickAdd = () => {
+		setPezzaCount(prev => prev + 1);
+	};
 
-  const onChangeParam = (index, setState) => {
-    setState(index);
-  };
+	const onChangeParam = (index, setState) => {
+		setState(index);
+	};
 
-  const { id, imageUrl, title, types, sizes, price, category, rating } = pizzaData;
+	const { id, imageUrl, title, types, sizes, price, category, rating } = pizzaData;
 
-  return (
-    <div className='pizza-block'>
-      <img className='pizza-block__image' src={imageUrl} alt={title} />
-      <h4 className='pizza-block__title'>{title}</h4>
-      <div className='pizza-block__selector'>
-        <ul>
-          {types.map(index => (
-            <li
-              key={index}
-              className={typeIndex === index ? 'active' : ''}
-              onClick={() => onChangeParam(index, setTypeIndex)}>
-              {typeNames[index]}
-            </li>
-          ))}
-        </ul>
-        <ul>
-          {sizes.map((sizeName, index) => (
-            <li
-              key={index}
-              className={sizeIndex === index ? 'active' : ''}
-              onClick={() => onChangeParam(index, setSizeIndex)}>
-              {sizeName} см.
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className='pizza-block__bottom'>
-        <div className='pizza-block__price'>от {price} ₽</div>
-        <div onClick={onClickAdd} className='button button--outline button--add'>
-          <svg
-            width='12'
-            height='12'
-            viewBox='0 0 12 12'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'>
-            <path
-              d='M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z'
-              fill='white'
-            />
-          </svg>
-          <span>Добавить</span>
-          {pizzaCount ? <i>{pizzaCount}</i> : null}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='pizza-block-wrapper'>
+			<div className='pizza-block'>
+				<img className='pizza-block__image' src={imageUrl} alt={title} />
+				<h4 className='pizza-block__title'>{title}</h4>
+				<div className='pizza-block__selector'>
+					<ul>
+						{types.map(index => (
+							<li
+								key={index}
+								className={typeIndex === index ? 'active' : ''}
+								onClick={() => onChangeParam(index, setTypeIndex)}
+							>
+								{typeNames[index]}
+							</li>
+						))}
+					</ul>
+					<ul>
+						{sizes.map((sizeName, index) => (
+							<li
+								key={index}
+								className={sizeIndex === index ? 'active' : ''}
+								onClick={() => onChangeParam(index, setSizeIndex)}
+							>
+								{sizeName} см.
+							</li>
+						))}
+					</ul>
+				</div>
+				<div className='pizza-block__bottom'>
+					<div className='pizza-block__price'>от {price} ₽</div>
+					<div onClick={onClickAdd} className='button button--outline button--add'>
+						<svg
+							width='12'
+							height='12'
+							viewBox='0 0 12 12'
+							fill='none'
+							xmlns='http://www.w3.org/2000/svg'
+						>
+							<path
+								d='M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z'
+								fill='white'
+							/>
+						</svg>
+						<span>Добавить</span>
+						{pizzaCount ? <i>{pizzaCount}</i> : null}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default PizzaBlock;
