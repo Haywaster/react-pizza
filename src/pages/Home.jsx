05643bbd2efcfe88b0c82632525e0bd508115a98
Haywaster <haywaster3@gmail.com>
2023-07-03 21:25:51 +0700

    second commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Categories from '../components/Categories';
-import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import Sceleton from '../components/PizzaBlock/Sceleton';
+import Sort from '../components/Sort';
 
 const Home = () => {
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+	const [items, setItems] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    fetch('https://63e33adf65ae4931770b0c4b.mockapi.io/ites')
-      .then(res => res.json())
-      .then(json => {
-        setItems(json);
-        setIsLoading(false);
-      });
-  }, []);
+	useEffect(() => {
+		fetch('https://64a2d760b45881cc0ae5c89c.mockapi.io/pizza')
+			.then(res => res.json())
+			.then(json => {
+				setItems(json);
+				setIsLoading(false);
+			});
+	}, []);
 
-  return (
-    <>
-      <div className='content__top'>
-        <Categories />
-        <Sort />
-      </div>
-      <h2 className='content__title'>Все пиццы</h2>
-      <div className='content__items'>
-        {isLoading
-          ? [...new Array(6)].map((_, index) => <Sceleton key={index} />)
-          : items.map((elem, index) => <PizzaBlock key={elem.id} {...elem} index={index} />)}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className='content__top'>
+				<Categories />
+				<Sort />
+			</div>
+			<h2 className='content__title'>Все пиццы</h2>
+			<div className='content__items'>
+				{isLoading
+					? [...new Array(6)].map((_, index) => <Sceleton key={index} />)
+					: items.map((elem, index) => <PizzaBlock key={elem.id} {...elem} index={index} />)}
+			</div>
+		</>
+	);
 };
 
 export default Home;
